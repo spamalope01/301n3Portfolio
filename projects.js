22f9389942137863a298ce807d2aece2367fb4c3
spamalope01 <spamalope01@yahoo.com>
2016-06-06 11:21:34 -0700

    got everything from class 1 and 2 working.  projects are rendering, tab on global nav works.  i'm still having an issue with images though.  however the system is showing a broken image placeholder.

--- projects.js
+++ projects.js
@@ -7,7 +7,7 @@ var oldProjects = [
     published: "5/31/2016",
     description: "<p>A web application directed at anyone who has medications they take or who acts as a caretaker for someone else who has several perscriptions.  This application allows users to maintain a running list of their medications, get reminders when they are overdue for taking a dose, indicate if they took or skipped a dose and receive positive reinforcement for taking their medications on time.</p>",
     tech: "HTML, CSS, JavaScript",
-    image:"IMGS/Image ONE.jpg",
+    image: "<img src='/IMGS/ImageONE.jpeg'/>",
     category: 'health'
   },
   {
@@ -17,7 +17,7 @@ var oldProjects = [
     published: "4/22/2016",
     description: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
     tech: "HTML, CSS, JavaScript",
-    image:"IMGS/Image TWO.jpg",
+    image: "<img src='/IMGS/ImageTWO.jpeg'/>",
     category: 'retail'
   },
   {
@@ -27,7 +27,7 @@ var oldProjects = [
     published: "10/2/2015",
     description: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
     tech: "HTML, CSS, JavaScript",
-    image:"IMGS/Image THREE.jpg",
+    image: "<img src='/IMGS/ImageTHREE.jpeg'/>",
     category: 'health'
   },
   {
@@ -37,7 +37,7 @@ var oldProjects = [
     published: "7/23/2015",
     description: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
     tech: "HTML, CSS, JavaScript",
-    image:"IMGS/Image FOUR.jpg",
+    image: "<img src='/IMGS/ImageFOUR.jpeg'/>",
     category: 'charity'
   },
 ];
