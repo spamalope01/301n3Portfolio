5debae15596eab7579b9e72a1b1bfcb489c0108b
spamalope01 <spamalope01@yahoo.com>
2016-06-05 20:14:41 -0700

    added more to the site, but i still can't get the clone to work or the tab functionality.

---
+++ projView.js
@@ -0,0 +1,15 @@
+var projControl = {};
+
+projControl.navDisplayToggle = function() {
+  $('.globalNav').on('click', '.tab', function() {
+    console.log('navToggle');
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).show();
+  });
+
+  // $('.globalNav .tab:first').click();
+};
+
+$(document).ready(function() {
+  projControl.navDisplayToggle();
+});
