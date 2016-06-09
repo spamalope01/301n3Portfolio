93f2af73eb8e0b890980325c228c38220bffc719
spamalope01 <spamalope01@yahoo.com>
2016-06-08 17:51:33 -0700

    got handlebars installed and working.  next is to smacss this thang.

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
-var myProj = [];
+// var myProj = [];
 
 
-function Projects (proj){
-  this.title = proj.title;
-  this.client = proj.client;
-  this.author = proj.author;
-  this.published = proj.published;
-  this.description = proj.description;
-  this.tech = proj.tech;
-  this.image = proj.image;
-  this.category = proj.category;
-}
-
-Projects.prototype.toHtml = function() {
-  var $newProject = $('section.template').clone();
-  $newProject.removeClass('template');
-  console.log('i cloned it');
-  $newProject.attr('data-category', this.category);
-  $newProject.attr('data-author', this.author);
-  $newProject.find('.projectTitle').text(this.title);
-  $newProject.find('.clientName').text(this.client);
-  $newProject.find('.devDate').text(this.published);
-  $newProject.find('.projectDescription').html(this.description);
-  $newProject.find('.techUsed').text(this.tech);
-  $newProject.find('.imageProj').html(this.image);
-  $newProject.append('<hr>');
-  return $newProject;
-  console.log($newProject);
-};
-
-oldProjects.forEach(function(ele) {
-  myProj.push(new Projects(ele));
+// function Projects (proj){
+//   this.title = proj.title;
+//   this.client = proj.client;
+//   this.author = proj.author;
+//   this.published = proj.published;
+//   this.description = proj.description;
+//   this.tech = proj.tech;
+//   this.image = proj.image;
+//   this.category = proj.category;
+// }
+$(function () {
+  var $templateScript = $('#myWork').html();
+  var theTemplate = Handlebars.compile($templateScript);
+  var compiledHtml = theTemplate(oldProjects);
+  $('#workDone').append(compiledHtml);
 });
+// Projects.prototype.toHtml = function() {
+//   var $newProject = $('section.template').clone();
+//   $newProject.removeClass('template');
+//   console.log('i cloned it');
+//   // $newProject.attr('data-category', this.category);
+//   // $newProject.attr('data-author', this.author);
+//   // $newProject.find('.projectTitle').text(this.title);
+//   // $newProject.find('.clientName').text(this.client);
+//   // $newProject.find('.devDate').text(this.published);
+//   // $newProject.find('.projectDescription').html(this.description);
+//   // $newProject.find('.techUsed').text(this.tech);
+//   // $newProject.find('.imageProj').html(this.image);
+//   $newProject.append('<hr>');
+//   return $newProject;
+//   console.log($newProject);
+// };
 
-myProj.forEach(function(a){
-  $('#workDone').append(a.toHtml());
-});
+// oldProjects.forEach(function(ele) {
+//   myProj.push(new Projects(ele));
+// });
+//
+// myProj.forEach(function(a){
+//   $('#workDone').append(a.toHtml());
+// });
 
 navDisplayToggle = function() {
   $('#globalNav').on('click', 'li', function() {
