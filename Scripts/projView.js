bd0369f61a7901062d770814d7a9e5f186e8aea8
spamalope01 <spamalope01@yahoo.com>
2016-06-19 16:06:36 -0700

    -got everything from last week to work.  json and ajax works finally.

--- Scripts/projView.js
+++ Scripts/projView.js
@@ -1,15 +1,33 @@
 var projControl = {};
 
+
 projControl.navDisplayToggle = function() {
   $('.globalNav').on('click', 'li', function() {
     console.log('navToggle');
     $('.tab-content').hide();
+    console.log($(this).data('content'));
     $('#' + $(this).data('content')).show();
   });
 
-  // $('.globalNav .tab:first').click();
+  $('.globalNav .tab:first').click();
+};
+
+projControl.navBurgerShow = function() {
+  $('.icon-menu').on('click', function() {
+    $('nav ul').toggle();
+  });
+};
+
+projControl.initIndexPage = function() {
+  console.log('initIndexPage is running');
+  Projects.all.forEach(function(a){
+    $('#workDone').append(a.toHtml());
+  });
 };
 
 $(document).ready(function() {
   projControl.navDisplayToggle();
+  projControl.navBurgerShow();
+  // projControl.initIndexPage();
+  Projects.fetchAll();
 });
